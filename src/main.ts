d54e8f527514ed731b5e1cb897ea02642ee11419
Marcone Boff <boffmarcone@gmail.com>
2026-01-21 15:59:47 -0300

    refactor: log

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
 
-  await app.listen(port);
-
-  Logger.log(`Application running on port ${port}`);
+  await app.listen(port, () => {
+    Logger.log(`Application running on port ${port}`);
+  });
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
